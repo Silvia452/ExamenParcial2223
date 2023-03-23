2942f9be23ba0c349a9a76d119ff986eedeb40a3
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-23 19:34:31 +0100

    Ejercicio 1

--- mates/src/main/java/mates/Principal.java
+++ mates/src/main/java/mates/Principal.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class Principal {
 
     public static void main(String[] args){
-        int puntos = 1000; // valor por defecto
-        if (args.length > 0) {
-            puntos = Integer.parseInt(args[0]);
-        }
-        System.out.println("El número PI es " + Matematicas.generarNumeroPi(puntos));
+        System.out.println("El número PI es " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
     }
 
 
