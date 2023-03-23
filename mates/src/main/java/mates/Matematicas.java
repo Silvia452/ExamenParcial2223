769c896fdea6bb1a30b7fcb8d84c4b74e34a1b10
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-23 19:19:51 +0100

    Terminado ejercicio 1

---
+++ mates/src/main/java/mates/Matematicas.java
@@ -0,0 +1,29 @@
+package mates;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Matematicas {
+    //esta dando pistas pero no se enterran
+
+    public static double generarNumeroPi(double pasos) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Inserte número de puntos a generar: ");
+        int puntosTotales = sn.nextInt();
+        int aciertos = 0;
+        double areaCuadrado = 4.0;
+
+        Random rnd = new Random(); //se hace random
+
+        for (int i = 1; i <= puntosTotales; i++) {
+            double x = rnd.nextDouble() * 2 - 1; // número aleatorio entre -1 y 1
+            double y = rnd.nextDouble() * 2 - 1; // número aleatorio entre -1 y 1
+            if (Math.sqrt(x * x + y * y) <= 1) {
+                aciertos++; // dentro del círculo se suman
+            }
+        }
+
+        return areaCuadrado * aciertos/puntosTotales;
+
+    }
+}
---
+++ mates/src/main/java/mates/Principal.java
@@ -0,0 +1,12 @@
+package mates;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args){
+        System.out.println("El número PI es " + Matematicas. generarNumeroPi(Integer.parseInt(args[0])));
+    }
+
+
+}
